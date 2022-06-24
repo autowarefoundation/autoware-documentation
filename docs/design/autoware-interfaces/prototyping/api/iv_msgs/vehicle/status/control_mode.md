35bf5aa3f6bd37c8bca9014548d306bdb1895394
Takagi, Isamu <43976882+isamu-takagi@users.noreply.github.com>
2022-06-24 18:27:46 +0900

    docs(prototyping-api): add iv msgs and start request (#23)

---
+++ docs/design/autoware-interfaces/prototyping/api/iv_msgs/vehicle/status/control_mode.md
@@ -0,0 +1,14 @@
+# /api/iv_msgs/vehicle/status/control_mode
+
+## Classification
+
+- Behavior: Topic
+- DataType: tier4_vehicle_msgs/msg/ControlMode
+
+## Description
+
+以前のフォーマットで Control Mode を取得する。
+
+## Requirement
+
+以前のフォーマットで Control Mode を提供すること。
--- tools/autoware_api_document/python/__init__.py
+++ tools/autoware_api_document/python/__init__.py
@@ -51,14 +51,13 @@ def generate():
 
 def generate_list(target, groups):
     with target.joinpath('index.md').open('w') as fp:
-        fp.write('# List of Autoware API\n\n')
+        fp.write('# List of TIER IV API\n\n')
         for group, specs in groups:
             table = MarkdownTable('Type', 'Name', 'Data')
             for spec in specs:
                 spec_name = make_page_link(spec)
                 type_name = make_type_link(spec)
                 table.line(spec.behavior, spec_name, type_name)
-            fp.write('## {} API\n\n'.format(group.capitalize()))
             fp.write(table.text() + '\n')
 
 
