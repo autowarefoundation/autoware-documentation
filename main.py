a9ed6f462d00d47503bff88d6be70d838f396861
h-ohta <hiroki.ota@tier4.jp>
2022-11-25 17:42:09 +0900

    feat: add url filter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os  # for relpath, cannot use pathlib
+import urllib
 
 
 def define_env(env):
@@ -8,3 +9,8 @@ def define_env(env):
         root_path = "design/autoware-interfaces/ad-api"
         base_path = os.path.relpath(root_path, env.page.url) + "/list"
         return f"[{name}]({base_path}{name})"
+
+    @env.filter
+    def drawio(image_path):
+        image_url = urllib.parse.quote(f"{env.conf['site_url']}{image_path}", "")
+        return f"https://app.diagrams.net/?lightbox=1#U{image_url}"
